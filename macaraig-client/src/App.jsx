aeaf3ea1e0c9b2228a8ac5b8f67a2fe6f4c6273b
navvvh <vhanhajjmacaraig21@gmail.com>
2026-03-11 13:36:57 +0800

    changing something

--- macaraig-client/src/App.jsx
+++ macaraig-client/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <h1>Welcome to Macaraig!</h1>
+        <h1>Welcome to My React WEB!</h1>
         <p>
-          Name: [Hajj Macaraig] <br />
-          Email: [vhanhajjmacaraig21@gmail.com] <br />
+          Name: Hajj Macaraig <br />
+          Email: vhanhajjmacaraig21@gmail.com <br />
           Other Personal Info: 
           <div>
             <a href="https://github.com/navvvh/macaraig-webprog.git" target="_blank" rel="noopener noreferrer">GitHub</a>
