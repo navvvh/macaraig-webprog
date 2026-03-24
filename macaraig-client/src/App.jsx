6610295d2e1289e7e7d79d4d59b3914fec4249d1
navvvh <vhanhajjmacaraig21@gmail.com>
2026-03-24 22:59:26 +0800

    initial commit

---
+++ macaraig-client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl font-bold text-purple-600'>
+      hello podfwdgwgqwrgqwrgegef
+    </div>
+  )
+}
+
+export default App
