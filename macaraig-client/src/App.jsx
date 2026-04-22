9ccefc89ad97ef600a933607dd82853a04e4b3bc
Hajj <vhanhajjmacariag21@gmail.com>
2026-04-22 13:24:29 +0800

    lab-act4

--- macaraig-client/src/App.jsx
+++ macaraig-client/src/App.jsx
@@ -1,63 +1,47 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Layout from './layouts/Layout';
-import ArticlePage from './pages/LandingPages/ArticlePage';
+import './index.css';
+import Layout from './components/Layout';
 import HomePage from './pages/LandingPages/HomePage';
 import AboutPage from './pages/LandingPages/AboutPage';
-import ArticleListPage from './pages/LandingPages/ArticleListPage';
-
-import AuthLayout from './layouts/AuthLayout';
-import SignInPage from './pages/AuthPages/SignInPage';
-import SignUpPage from './pages/AuthPages/SignUpPage';
-
+import ArticleListPage from "./pages/LandingPages/ArticleListPage";
+import ArticlePage from './pages/LandingPages/ArticlePage';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFoundPage from './pages/NotFoundPage';
 
-const routes = [
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: '',
-        element: <HomePage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'articles',
-        element: <ArticleListPage />,
-      },
-      {
-        path: 'articles/:name',
-        element: <ArticlePage />,
-      },
-    ],
-  },
-  {
-    path: "auth/",
-    element: <AuthLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "signin",
-        element: <SignInPage />,
-      },
-      {
-        path: "signup",
-        element: <SignUpPage />,
-      },
-    ],
-  },
-];
+const routes = [{
+  path: '/',
+  element: <Layout />,
+  errorElement: <NotFoundPage />,
+  children: [
+    {
+      path: '/',
+      element: <HomePage />
+    },
+    {
+      path: '/about',
+      element: <AboutPage />
+    },
+    {
+      path: '/articles',
+      element: <ArticleListPage />
+    },
+    {
+      path: '/articles/:name',
+      element: <ArticlePage />
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />
+    }
+  ]
+}];
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
