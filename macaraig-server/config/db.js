cf2bac8737b6359aa62fa7691fe73862874d2267
Hajj <vhanhajjmacariag21@gmail.com>
2026-05-29 19:52:30 +0800

    migrating to atlas - doing deployment

--- macaraig-server/config/db.js
+++ macaraig-server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log('URI:', process.env.MONGODB_URI);
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
